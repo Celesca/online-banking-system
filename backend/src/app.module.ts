c086dcc3594ff77495b1c6ba761d7a91a5e48b10
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-04 01:50:03 +0700

    add: sqlite connection and typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './app.sqlite',
+      entities: [],
+      synchronize: process.env.NODE_ENV != 'production',
+    }),
+  ],
 })
 export class AppModule {}
