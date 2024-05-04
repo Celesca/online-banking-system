557b01b1d797b97611c0db7280103e68ad6a1e72
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-04 21:37:54 +0700

    fix: unused imports from lint

--- backend/src/loan-type/loan-type.module.ts
+++ backend/src/loan-type/loan-type.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoanTypeService } from './loan-type.service';
 import { LoanTypeController } from './loan-type.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoanType } from './entities/loan-type.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LoanType])],
   controllers: [LoanTypeController],
   providers: [LoanTypeService],
 })
