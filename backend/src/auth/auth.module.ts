275e7e7c3503cd3ff372b2a14c7a237be2229048
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-05 12:10:19 +0700

    fix: husky and unused var

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
+  imports: [UsersService],
   controllers: [AuthController],
   providers: [AuthService],
 })
