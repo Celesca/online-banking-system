40ebcd676f3174b32edc15f1f28fc238e76ff36d
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-06 02:21:23 +0700

    fix: entities

--- backend/src/customers/entities/customer.entity.ts
+++ backend/src/customers/entities/customer.entity.ts
@@ -1,21 +1,34 @@
 import { Person } from 'src/persons/entities/person.entity';
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   OneToOne,
-  PrimaryGeneratedColumn,
+  PrimaryColumn,
 } from 'typeorm';
 
 @Entity()
 export class Customer {
-  @PrimaryGeneratedColumn()
-  customer_id: number;
+  @PrimaryColumn({ type: 'string', length: 30 })
+  customer_username: string;
+
+  @Column({ type: 'string', length: 20 })
+  customer_password: string;
 
   @Column({ type: 'int', nullable: false })
   customer_salary: number;
 
   @OneToOne(() => Person)
-  @JoinColumn({ name: 'person_id' })
-  person: Person;
+  @JoinColumn({ name: 'nation_card_id' })
+  national_card_id: Person;
+
+  @Column({ type: 'boolean', default: true })
+  status: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
 }
