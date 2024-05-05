b375508ee247d728641d0a730cd9169962d53a9b
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-05 12:44:39 +0700

    add: LocalAuth passport.js

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,16 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private userService: UsersService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
-    const user = await this.userService.findOne(username);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      // const { password, ...result } = user;
+      // return result;
     }
-
-    const payload = { sub: user.user_id, username: user.username };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+    return null;
   }
 }
