c2429641097c5118cd3ef390779e8009064fae00
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-05 12:01:43 +0700

    format: user service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -5,14 +5,12 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-    constructor (
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>
-    ) {}
-
-    async findOne(username: string): Promise<User | undefined> {
-        return this.userRepository.findOne({ where: { username : username}})
-    }
-
+  async findOne(username: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { username: username } });
+  }
 }
