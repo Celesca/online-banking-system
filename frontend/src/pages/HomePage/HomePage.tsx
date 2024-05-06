f0fca0425f065be23e9cd4a5b9285a8d845f097e
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-06 17:34:43 +0700

    add: Login Button

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -7,11 +7,14 @@ const HomePage = () => {
         <div className="pt-16 text-center">
           <h1 className="head_text text-white">TWJ Online Banking</h1>
         </div>
-        <button className="login-button relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        <div className="login-container pt-16">
+        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 focus:delay-300 transition delay-100 duration-300 ease-in-out font-medium rounded-lg text-sm px-12 py-4 text-center me-2 mb-2">
           <Link to="/login">Login</Link>
-          </span>
         </button>
+          <div className="text-white text-lg m-12">
+          don't have account? <Link to="/register" className="underline decoration-solid text-blue-900">Sign up</Link>
+          </div>
+        </div>
         
     </div>
   )
