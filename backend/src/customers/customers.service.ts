1eec9561ba8ab4a63ec4ef7179b680aa2005e468
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-06 12:52:58 +0700

    add: checkId

--- backend/src/customers/customers.service.ts
+++ backend/src/customers/customers.service.ts
@@ -3,15 +3,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Customer } from './entities/customer.entity';
 import { CustomerRegisterDto } from './dto/customer-register.dto';
+import { Person } from 'src/persons/entities/person.entity';
 
 @Injectable()
 export class CustomersService {
   constructor(
     @InjectRepository(Customer)
     private customerRepository: Repository<Customer>,
+    @InjectRepository(Person)
+    private personRepository: Repository<Person>,
   ) {}
 
+  async checkId(nation_card_id) {
+    const existedPerson = await this.personRepository.findOneBy({
+      nation_card_id,
+    });
+    if (existedPerson) {
+      return true;
+    }
+    throw new HttpException('No person found', 404);
+  }
+
   async create(body: CustomerRegisterDto) {
+    // Find the person with nation card id first
     const existedUser = await this.customerRepository.findOneBy({
       customer_username: body.username,
     });
