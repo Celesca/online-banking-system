d0997fb85f298c123a770e6d0d24d7941ec06739
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-06 17:21:17 +0700

    add: homePage

--- backend/src/customers/customers.controller.ts
+++ backend/src/customers/customers.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Delete,
-  Post,
-  HttpCode,
-  Body,
-} from '@nestjs/common';
+import { Controller, Post, HttpCode, Body } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomerRegisterDto } from './dto/customer-register.dto';
 
@@ -24,20 +16,5 @@ export class CustomersController {
   @HttpCode(201)
   create(@Body() body: CustomerRegisterDto) {
     return this.customersService.create(body);
-  }
-
-  @Get()
-  findAll() {
-    return this.customersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.customersService.findOne(id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customersService.remove(id);
   }
 }
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom"
+import './HomePage.css'
 
 const HomePage = () => {
   return (
-    <div>
-        <div>HomePage</div>
-        <Link to="/login">Login</Link>
+    <div className="bg-gradient-to-r from-indigo-500 homepage_container">
+        <div className="pt-16 text-center">
+          <h1 className="head_text text-white">TWJ Online Banking</h1>
+        </div>
+        <button className="login-button relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          <Link to="/login">Login</Link>
+          </span>
+        </button>
+        
     </div>
   )
 }
